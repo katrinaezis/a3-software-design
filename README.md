4fa00fb9a1a16b8716af31a88b70d2a8df68d760
iGuest <katrinaezis@gmail.com>
2017-05-18 02:01:14 -0700

    donezo

--- README.md
+++ README.md
@@ -1,2 +1,108 @@
-# a3-software-design
-Reusable chart
+# :doughnut: An Adoughable Donut Chart :doughnut:
+donut.js is a JS + D3 library capable of 
+visualizing all the data in a super wonderful, 
+state-of-the-art donut chart.
+But doughnut just take it from me, check it out for yourself!
+
+## Getting Started
+In order for this ish to work, you gotta get a hole bunch of stuff.
+In the `<head>` of your index.html put the following:
+```html
+<!-- jquery 4 dayz -->
+<script src="https://code.jquery.com/jquery.min.js"></script>
+
+<!-- d3 4 u -->
+<script src="https://d3js.org/d3.v4.min.js"></script>
+
+<!-- the cream filling aka my library -->
+<script src="js/donut.js"></script>
+
+<!-- I like my graphs pretty -->
+<script src="js/main.js"></script>
+```
+
+## Dat Data Dough 
+donut.js uses any sort of data, alls you need the
+name of what you want to visualize (category) and its
+value (variable). That way you can take in dope data like this:
+```
+name, value
+Katrina, 150
+Mike, 200
+Vincent, 130
+Life, 0
+MyGrade, 0
+```
+and chart that ish. We just need to put a lil prep in 
+your step and you'll be off to the races!
+```javascript
+d3.csv('../path/to/data', function(data) {
+        d3.select('#yourDiv')
+            .datum(data) // bind data
+            .call(donut); // draw 
+    });
+ ```
+ If you have lots of properties you'll have to tell
+ donut what's what like this:
+ ```javascript
+  var donut = donutChart()
+        .variable('value')
+        .category('name');
+```
+This tells donut which properties to look at. Helpful 
+for huge ass data sets. 
+
+## Gettin' Artsy Af
+You're pretty much ready to draw your graph, 
+just throw it in a function, hook it up to your main and 
+you're golden.
+```javascript
+$(function() {
+     var donut = donutChart()
+        .width($('#vis').parent().width() - 100) // recommended addition
+        .height(500) // also recommended
+        .variable('value') // we learned this!
+        .category('name') // and this!
+        .title('donuts 4 dayz'); // dope title for a dope graph
+
+    d3.csv('../path/to/data', function(data) { //back at it with the data
+        d3.select('#yourDiv')
+            .datum(data) // bind data
+            .call(donut); // draw 
+    });
+});
+```
+
+## Choose your own adventure
+
+#### .title 
+P self explanatory. Set your title to something
+dope, donut let me down!
+
+#### .width
+Does what you think it does. Sets the width
+of your graph.
+
+#### .height
+Man I really made this easy on you. Sets the height
+of your graph.
+
+#### .radius
+What is the plural form of radius? Radi? Radii?
+Well set all your radii here!
+
+#### .color
+Put some sprinkles on your donut! This changes the color 
+scheme.
+
+#### .variable 
+We explained this one earlier, but incase u a dum-dum here it 
+is again: variable allows you to choose which parameter to take
+is as your % of the donut value.
+
+#### .category
+I swear I explained this one too! But whatevs, this sets the names
+of your donut pieces. That was it's v easy to switch between 
+parameters and all that jazz.  
+
+## You done boo! Go forth and :doughnut: 
--- js/donut.js
+++ js/donut.js
@@ -1,188 +1,29 @@
-// var donut  = function() {
-//     var width = 960,
-//     height = 500,
-//     radius = Math.min(width, height) / 2,
-//      margin = {
-//                 left: 70,
-//                 bottom: 50,
-//                 top: 30,
-//                 right: 10,
-//             },
-//         color = d3.scaleOrdinal(d3.schemeCategory20c),
-//         title = 'Example Title',
-//         delayNum = 900;
-
-//     var chart = function(selection) {
-//         var arc = d3.svg.arc()
-//             .outerRadius(radius - 10)
-//             .innerRadius(radius - 70);
-        
-//         var outerArc = d3.arc()
-// 			.outerRadius(radius - 5)
-// 			.innerRadius(radius - 5);
-
-//         var pie = d3.layout.pie()
-//             .sort(null)
-//             .value(function(d) { return d.value; });
-
-//         selection.each(function(data) {
-//             console.log(data);
-//             var ele = d3.select(this);
-//             var svg = ele.selectAll("svg").data([data]);
-
-//             var svgEnter = svg.enter()
-//                 .append("svg")
-//                 .attr('width', width + margin.left + margin.right)
-//                 .attr("height", height + margin.top + margin.bottom)
-            
-//             svg.append('text')
-//                 .attr('transform', 'translate(' + (width  / 2) + ',' + 20 + ')')
-//                 .text(title)
-//                 .attr('class', 'chart-title');
-
-// 			svgEnter.append('g')
-// 				.attr('class', 'sliceLabel')
-// 				.attr('width', width)
-// 				.attr('height', height)
-// 				.attr('transform', 'translate(' + (width / 2) + ',' + height / 2 + ')');
-
-// 			svgEnter.append('g')
-// 				.attr('transform', 'translate(' + (width  / 2) + ',' + height/2 + ')')
-// 				.attr('class', 'chartG');
-
-// 			svgEnter.append('g')
-// 				.attr('class', 'lines')
-// 				.attr('transform', 'translate(' + (width  / 2) + ',' + height/2 + ')');
-            
-//             var path = ele.select('.chartG').selectAll('path')
-//                 .data(pie(data[0]))
-// 				.enter().append('path')
-// 				.attr('fill', function(d) { return color(d.data.key); })
-// 				.transition()
-// 				.delay(function(d,i) { return i * 800; })
-// 				.duration(800)
-// 				.attrTween('d', function(d) {
-// 					var i = d3.interpolate(d.startAngle+0.1, d.endAngle);
-// 					return function(t) {
-// 						d.endAngle = i(t);
-// 						return arc(d);
-// 					}
-// 				});
-            
-//             var label = ele.select('.sliceLabel').selectAll('text')
-// 				.data(pie(data[0])).enter()
-// 				.append('text')
-// 				//.attr('transform', function(d) {return 'translate(' + arc.centroid(d) + ')'; })
-// 				.transition()
-// 				.delay(function(d,i) { return i * 800; })
-// 				.attr('transform', labelTransform)
-// 				.attr('dy', '.35em')
-// 				//.text(function(d) { return d.data.key; })
-// 				.text(function(d) { return (d.data.key) + ': ' + (d.data.value); })
-// 				.style('fill', '#000')
-// 				.style('text-anchor', function(d) {
-// 					return (midAngle(d)) < Math.PI ? 'start' : 'end';
-// 				});
-
-//             function labelTransform(d, i) {
-// 				var pos = outerArc.centroid(d);
-// 				pos[0] = radius * 0.95 * (midAngle(d) < Math.PI ? 1: -1);
-// 				return 'translate(' + pos + ')';
-// 			};
-
-//             function midAngle(d) { return d.startAngle + (d.endAngle - d.startAngle) / 2; }
-			
-// 			// Add lines connecting text labels to the slices of the donut chart
-// 			var polyline = ele.select('.lines').selectAll('polyline')
-// 				.data(pie(data[0]))
-// 				.enter().append('polyline')
-// 				.transition()
-// 				.delay(function(d,i) { return i * 800; })
-// 				.attr('points', calculatePoints)
-// 				.attr('fill', 'none')
-// 				.attr('stroke-width', 1)
-// 				.attr('stroke', '#808080');
-
-// 			// Caculate the exact position to draw polylines
-// 			function calculatePoints(d) {
-// 				var pos = outerArc.centroid(d);
-// 				pos[0] = radius * 0.95 * (midAngle(d) < Math.PI ? 1 : -1);
-// 				return [arc.centroid(d), outerArc.centroid(d), pos];
-// 			}
-
-                
-
-//             // ele.select('chartG')
-//             //     .data(pie(data)[0])
-//             //     .enter().append("g")
-//             //     .attr("class", "arc");
-            
-//             // ele.select("chartG").append("path")
-//             //     .attr("d", arc)
-//             //     .style("fill", function(d) { return color(d.data[0]);});
-            
-//             // ele.select("chartG").append("text")
-//             //     .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
-//             //     .attr("dy", ".35em")
-//             //     .text(function(d) { return d.data[1]; });
-            
-//         });
-
-//     }
-
-//     chart.height = function(value) {
-// 		if (!argument.length) return height;
-// 		height = value;
-// 		return chart;
-// 	};
-
-// 	chart.width = function(value) {
-// 		if (!argument.length) return width;
-// 		width = value;
-// 		return chart;
-// 	};
-
-// 	chart.radius = function(value) {
-// 		if (!argument.length) return radius;
-// 		radius = value;
-// 		return chart;
-// 	};
-
-// 	chart.color = function(value) {
-// 		if (!argument.length) return color;
-// 		color = value;
-// 		return chart;
-// 	};
-
-// 	chart.title = function(value) {
-// 		if (!argument.length) return title;
-// 		tile = value;
-// 		return chart;
-// 	};
-//     return chart;
-
-    
-// };
-
+// Katrina Ezis
+// INFO474 Donut Chart Reusable Example
+// Adopted from http://bit.ly/2qUix6I
+// as well as from http://bit.ly/1qUOxB1
+// as well as from http://bit.ly/2qU9oL
 function donutChart() {
 	var width = 960,
     height = 500,
-	radius = Math.min(width, height) / 2,
+	radius,
     margin = {
 		left: 10,
 		bottom: 30,
-		top: 10,
+		top: 100,
 		right: 10,
 	},
 	color = d3.scaleOrdinal(d3.schemeCategory20c),
 	variable,
+	title = 'Dank Donuts',
 	category,
-	padAngle,
 	floatFormat = d3.format('.4r'),
-	cornerRadius,
 	percentFormat = d3.format(',.2%');
 
 	function chart(selection) {
+		var chartHeight = height - margin.bottom - margin.top;
+        var chartWidth = width - margin.left - margin.right;
+		var radius = Math.min(chartWidth, chartHeight) /2;
 		selection.each(function(data) {
 			var pie = d3.pie()
 				.value(function(d) {
@@ -192,41 +33,53 @@ function donutChart() {
 			
 			var arc = d3.arc()
 				.outerRadius(radius * 0.8)
-				.innerRadius(radius * 0.6)
-				.cornerRadius(cornerRadius)
-				.padAngle(padAngle);
+				.innerRadius(radius * 0.6);
 			
 			var outerArc = d3.arc()
 				.outerRadius(radius * 0.9)
 				.innerRadius(radius * 0.9);
-			
-			var svg = selection.append('svg')
-				.attr('width', width + margin.left + margin.right)
-				.attr('height', height + margin.top + margin.bottom)
-				.append('g')
-					.attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
-			
-			svg.append('g').attr('class', 'slices');
-			svg.append('g').attr('class', 'labelName');
-			svg.append('g').attr('class', 'lines');
 
-			var path = svg.select('.slices')
+			var ele = d3.select(this);
+			var svg = ele.selectAll('svg').data([data]);
+
+			var svgEnter = svg.enter()
+				.append("svg")
+				.attr('width', chartWidth)
+				.attr('height', chartHeight)
+				.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+			
+			svgEnter.append('text')
+				.attr('transform', 'translate(' + ((chartWidth - margin.left) / 2) + ',' + 20 + ')')
+                .text(title)
+                .attr('class', 'chart-title');
+			
+			 svgEnter.append('g')
+                    .attr('transform', 'translate(' + chartWidth/2 + "," + chartHeight/2 + ')')
+                    .attr('class', 'chart-g');
+
+			svgEnter.append('g').attr('class', 'slices')
+				.attr("transform", "translate(" + width/2 + "," + height/2 + ")");
+			svgEnter.append('g').attr('class', 'labelName')
+				.attr("transform", "translate(" + width/2 + "," + height/2 + ")");
+			svgEnter.append('g').attr('class', 'lines')
+				.attr("transform", "translate(" + width/2 + "," + height/2 + ")");
+			
+			var path = ele.select('.slices')
 				.datum(data).selectAll('path')
 				.data(pie)
 				.enter().append('path')
 					.attr('fill', function(d) { 
 							return color(d.data[category]);
 						})
-					.attr('d', arc);
-			
-			
-			var label = svg.select('.labelName').selectAll('text')
+					.attr('d', arc)
+
+			var label = ele.select('.labelName').selectAll('text')
 				.data(pie)
 				.enter().append('text')
 					.attr('dy', '.35em')
+					
 					.html(function(d) {
-						return d.data[category] + ': <tspan>' + percentFormat(d.data[variable]) +
-							'</tspan>';
+						return d.data[category] + ': ' + percentFormat(d.data[variable]);
 					})
 					.attr('transform', function(d) {
 						var pos = outerArc.centroid(d)
@@ -237,7 +90,7 @@ function donutChart() {
 						return (midAngle(d)) < Math.PI ? 'start' : 'end'
 					})
 			
-			var polyline = svg.select('.lines')
+			var polyline = ele.select('.lines')
 				.selectAll('polyline')
 				.data(pie)
 				.enter().append('polyline')
@@ -256,18 +109,15 @@ function donutChart() {
 			function toolTip(selection) {
 				var tip = '',
 					i = 0;
-				for (var key in data.data) {
-					var value = (!isNaN(parseFloat(data.data[key]))) ? percentFormat(data.data[key]) : data.data[key];
-					if (i == 0) {
-						tip += '<tspan x="0">' + key + ': ' + value + '</tspan>';
-					} else { 
-						tip += '<tspan x="0" dy="1.2em">' + key + ': ' + value + '</tspan>';
-					}
-					i++;
-				}
 			}
 		});
 	}
+	chart.title = function(value) {
+		if (!arguments.length) return title;
+        title = value;
+        return chart;
+	}
+
 	chart.width = function(value) {
         if (!arguments.length) return width;
         width = value;
@@ -280,33 +130,15 @@ function donutChart() {
         return chart;
     };
 
-    chart.margin = function(value) {
-        if (!arguments.length) return margin;
-        margin = value;
-        return chart;
-    };
-
     chart.radius = function(value) {
         if (!arguments.length) return radius;
         radius = value;
         return chart;
     };
 
-    chart.padAngle = function(value) {
-        if (!arguments.length) return padAngle;
-        padAngle = value;
-        return chart;
-    };
-
-    chart.cornerRadius = function(value) {
-        if (!arguments.length) return cornerRadius;
-        cornerRadius = value;
-        return chart;
-    };
-
-    chart.colour = function(value) {
-        if (!arguments.length) return colour;
-        colour = value;
+    chart.color = function(value) {
+        if (!arguments.length) return color;
+        color = value;
         return chart;
     };
 
@@ -321,6 +153,8 @@ function donutChart() {
         category = value;
         return chart;
     };
+
+	
 
     return chart;
 }
